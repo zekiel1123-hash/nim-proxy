bb32e644bc67305cf26999df6af743a7c4db3d2e
zekiel1123-hash <zekiel1123@gmail.com>
2026-05-07 15:20:01 -0700

    Update server.js

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.post('/v1/chat/completions', async (req, res) => {
       messages: messages,
       temperature: temperature || 0.6,
       max_tokens: max_tokens || 9024,
-      extra_body: ENABLE_THINKING_MODE ? { chat_template_kwargs: { thinking: true } } : undefined,
+      extra_body: ENABLE_THINKING_MODE ? { chat_template_kwargs: { thinking: true, reasoning_effort:"max" } } : undefined,
       stream: stream || true
     };
     
